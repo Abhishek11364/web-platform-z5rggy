306c93713f1f3275b898a6d93e785afe38efc361
Abhishek11364 <git@stackblitz.com>
2024-10-10 18:35:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-z5rggy
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Abhishek11364/web-platform-z5rggy)
---
+++ script.js
@@ -0,0 +1,81 @@
+let p = document.createElement('p');
+document.body.appendChild(p);
+
+const ip = document.querySelector("#ipuser");
+const h=document.querySelector('h1');
+// // Listen for 'keypress' event
+// ip.addEventListener('keypress', (e) => {
+//   console.log(`Key pressed: ${e.key}`);  // Logs the key pressed (e.g., "a", "b", etc.)
+//   console.log(e); 
+//   console.log(e.code);
+//    // Logs the entire event object to show more details
+// });
+// ip.addEventListener('keyup',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('keydown',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+  
+// })
+// ip.addEventListener('mousedown',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('mouseup',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('click',(e)=>{   // one down+up
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('mouseenter',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('mouseleave',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('mousemove',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('mouseout',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('mouseover',(e)=>{  differce betwwen mouseover and mouseenter
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('wheel',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('scroll',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('touchstart',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('touchend',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('touchmove',(e)=>{
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// h.addEventListener('drag',(e)=>{ //pnly in desktop
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
+// ip.addEventListener('pointerenter',(e)=>{   //provide functinality in both desktop and phone
+//   console.log('COde: ',e.code);
+//   console.log('Value', e.key);
+// })
